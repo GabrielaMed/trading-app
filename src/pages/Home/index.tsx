13dd866b3126ed9cd0f209a4abd72a66ef421d8c
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-02-07 14:36:48 -0400

    refactor: filter cotacao undefined and change yellow and green order

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -88,40 +88,43 @@ export const Home = () => {
               </tr>
             </thead>
             <tbody>
-              {data.map((ticker, idx) => (
-                <tr key={idx}>
-                  <td>{ticker.name}</td>
-                  <td>{ticker.cotacao}</td>
+              {data
+                .filter((ticker) => ticker.cotacao !== undefined)
+                .map((ticker, idx) => (
+                  <tr key={idx}>
+                    <td>{ticker.name}</td>
+                    <td>{ticker.cotacao}</td>
 
-                  {fields.find((field) => field.name === 'B. Graham')
-                    ?.display && (
-                    <td
-                      style={{
-                        backgroundColor:
-                          +formulateBGraham(ticker.lpa, ticker.vpa) <
-                          ticker.cotacao
-                            ? colors.lightYellow
-                            : colors.lightGreen,
-                      }}
-                    >
-                      {formulateBGraham(ticker.lpa, ticker.vpa)}
-                    </td>
-                  )}
+                    {fields.find((field) => field.name === 'B. Graham')
+                      ?.display && (
+                      <td
+                        style={{
+                          backgroundColor:
+                            +formulateBGraham(ticker.lpa, ticker.vpa) >
+                            ticker.cotacao
+                              ? colors.lightGreen
+                              : colors.lightYellow,
+                        }}
+                      >
+                        {formulateBGraham(ticker.lpa, ticker.vpa)}
+                      </td>
+                    )}
 
-                  {fields.find((field) => field.name === 'Bazin')?.display && (
-                    <td
-                      style={{
-                        backgroundColor:
-                          +formulateBazin(ticker.cotacao, ticker.dy) <
-                          ticker.cotacao
-                            ? colors.lightYellow
-                            : colors.lightGreen,
-                      }}
-                    >
-                      {formulateBazin(ticker.cotacao, ticker.dy)}
-                    </td>
-                  )}
-                  {/* 
+                    {fields.find((field) => field.name === 'Bazin')
+                      ?.display && (
+                      <td
+                        style={{
+                          backgroundColor:
+                            +formulateBazin(ticker.cotacao, ticker.dy) >
+                            ticker.cotacao
+                              ? colors.lightGreen
+                              : colors.lightYellow,
+                        }}
+                      >
+                        {formulateBazin(ticker.cotacao, ticker.dy)}
+                      </td>
+                    )}
+                    {/* 
                 {fields.find((field) => field.name === 'MSeg. Graham')
                   ?.display && (
                   <td>
@@ -133,155 +136,152 @@ export const Home = () => {
                   </td>
                 )} */}
 
-                  {fields.find((field) => field.name === 'CAGR')?.display && (
-                    <td>{ticker.cagr}</td>
-                  )}
+                    {fields.find((field) => field.name === 'CAGR')?.display && (
+                      <td>{ticker.cagr}</td>
+                    )}
 
-                  {fields.find((field) => field.name === 'DY')?.display && (
-                    <td>{ticker.dy}</td>
-                  )}
+                    {fields.find((field) => field.name === 'DY')?.display && (
+                      <td>{ticker.dy}</td>
+                    )}
 
-                  {fields.find((field) => field.name === 'Growth')?.display && (
-                    <td>{ticker.growth}</td>
-                  )}
+                    {fields.find((field) => field.name === 'Growth')
+                      ?.display && <td>{ticker.growth}</td>}
 
-                  {fields.find((field) => field.name === 'Have Date')
-                    ?.display && <td>{ticker.haveDate}</td>}
+                    {fields.find((field) => field.name === 'Have Date')
+                      ?.display && <td>{ticker.haveDate}</td>}
 
-                  {fields.find((field) => field.name === 'LPA')?.display && (
-                    <td>{ticker.lpa}</td>
-                  )}
+                    {fields.find((field) => field.name === 'LPA')?.display && (
+                      <td>{ticker.lpa}</td>
+                    )}
 
-                  {fields.find((field) => field.name === 'D/ebitida')
-                    ?.display && (
-                    <td
-                      style={{
-                        backgroundColor:
-                          ticker.debitOfEbitida < 3.5
-                            ? colors.lightYellow
-                            : colors.lightGreen,
-                      }}
-                    >
-                      {ticker.debitOfEbitida}
-                    </td>
-                  )}
+                    {fields.find((field) => field.name === 'D/ebitida')
+                      ?.display && (
+                      <td
+                        style={{
+                          backgroundColor:
+                            ticker.debitOfEbitida > 3.4
+                              ? colors.lightGreen
+                              : colors.lightYellow,
+                        }}
+                      >
+                        {ticker.debitOfEbitida}
+                      </td>
+                    )}
 
-                  {fields.find((field) => field.name === 'Margem Liq.')
-                    ?.display && (
-                    <td
-                      style={{
-                        backgroundColor:
-                          ticker.mliquida < 12
-                            ? colors.lightYellow
-                            : colors.lightGreen,
-                      }}
-                    >
-                      {ticker.mliquida}
-                    </td>
-                  )}
+                    {fields.find((field) => field.name === 'Margem Liq.')
+                      ?.display && (
+                      <td
+                        style={{
+                          backgroundColor:
+                            ticker.mliquida > 12
+                              ? colors.lightGreen
+                              : colors.lightYellow,
+                        }}
+                      >
+                        {ticker.mliquida}
+                      </td>
+                    )}
 
-                  {fields.find((field) => field.name === 'Payout')?.display && (
-                    <td>{ticker.payout}</td>
-                  )}
+                    {fields.find((field) => field.name === 'Payout')
+                      ?.display && <td>{ticker.payout}</td>}
 
-                  {fields.find((field) => field.name === 'PL Médio')
-                    ?.display && (
-                    <td
-                      style={{
-                        backgroundColor:
-                          +formulatePlMedio(
-                            +ticker.pl1,
-                            +ticker.pl2,
-                            +ticker.pl3,
-                            +ticker.pl4
-                          ) < ticker.pl
-                            ? colors.lightYellow
-                            : colors.lightGreen,
-                      }}
-                    >
-                      {formulatePlMedio(
-                        +ticker.pl1,
-                        +ticker.pl2,
-                        +ticker.pl3,
-                        +ticker.pl4
-                      )}
-                    </td>
-                  )}
+                    {fields.find((field) => field.name === 'PL Médio')
+                      ?.display && (
+                      <td
+                        style={{
+                          backgroundColor:
+                            +formulatePlMedio(
+                              +ticker.pl1,
+                              +ticker.pl2,
+                              +ticker.pl3,
+                              +ticker.pl4
+                            ) > ticker.pl
+                              ? colors.lightGreen
+                              : colors.lightYellow,
+                        }}
+                      >
+                        {formulatePlMedio(
+                          +ticker.pl1,
+                          +ticker.pl2,
+                          +ticker.pl3,
+                          +ticker.pl4
+                        )}
+                      </td>
+                    )}
 
-                  {fields.find((field) => field.name === 'PL')?.display && (
-                    <td>{ticker.pl}</td>
-                  )}
+                    {fields.find((field) => field.name === 'PL')?.display && (
+                      <td>{ticker.pl}</td>
+                    )}
 
-                  {fields.find((field) => field.name === 'PVP')?.display && (
-                    <td>{ticker.pvp}</td>
-                  )}
+                    {fields.find((field) => field.name === 'PVP')?.display && (
+                      <td>{ticker.pvp}</td>
+                    )}
 
-                  {fields.find((field) => field.name === 'Sector')?.display && (
-                    <td>{ticker.sector}</td>
-                  )}
+                    {fields.find((field) => field.name === 'Sector')
+                      ?.display && <td>{ticker.sector}</td>}
 
-                  {fields.find((field) => field.name === 'Tag Along')
-                    ?.display && <td>{ticker.tagAlong}</td>}
+                    {fields.find((field) => field.name === 'Tag Along')
+                      ?.display && <td>{ticker.tagAlong}</td>}
 
-                  {fields.find((field) => field.name === 'VPA')?.display && (
-                    <td>{ticker.vpa}</td>
-                  )}
+                    {fields.find((field) => field.name === 'VPA')?.display && (
+                      <td>{ticker.vpa}</td>
+                    )}
 
-                  {fields.find((field) => field.name === 'PL 1')?.display && (
-                    <td>{!ticker.pl1 ? '0.00' : ticker.pl1}</td>
-                  )}
+                    {fields.find((field) => field.name === 'PL 1')?.display && (
+                      <td>{!ticker.pl1 ? '0.00' : ticker.pl1}</td>
+                    )}
 
-                  {fields.find((field) => field.name === 'PL 2')?.display && (
-                    <td>{!ticker.pl2 ? '0.00' : ticker.pl2}</td>
-                  )}
+                    {fields.find((field) => field.name === 'PL 2')?.display && (
+                      <td>{!ticker.pl2 ? '0.00' : ticker.pl2}</td>
+                    )}
 
-                  {fields.find((field) => field.name === 'PL 3')?.display && (
-                    <td>{!ticker.pl3 ? '0.00' : ticker.pl3}</td>
-                  )}
+                    {fields.find((field) => field.name === 'PL 3')?.display && (
+                      <td>{!ticker.pl3 ? '0.00' : ticker.pl3}</td>
+                    )}
 
-                  {fields.find((field) => field.name === 'PL 4')?.display && (
-                    <td>{!ticker.pl4 ? '0.00' : ticker.pl4}</td>
-                  )}
+                    {fields.find((field) => field.name === 'PL 4')?.display && (
+                      <td>{!ticker.pl4 ? '0.00' : ticker.pl4}</td>
+                    )}
 
-                  {fields.find((field) => field.name === 'ROE')?.display && (
-                    <td>{ticker.roe}</td>
-                  )}
-                  {fields.find((field) => field.name === 'Sinal Aluguel')
-                    ?.display && (
-                    <td align='center'>
-                      {formulateRentSign(
-                        ticker.qttyRent1,
-                        ticker.qttyRent2,
-                        ticker.qttyRent3,
-                        ticker.rentAverage
-                      ) ? (
-                        <MdOutlineCheckCircleOutline
-                          color={colors.green}
-                          size={24}
-                        />
-                      ) : (
-                        <MdOutlineCancel color={colors.red} size={24} />
-                      )}
-                    </td>
-                  )}
+                    {fields.find((field) => field.name === 'ROE')?.display && (
+                      <td>{ticker.roe}</td>
+                    )}
+                    {fields.find((field) => field.name === 'Sinal Aluguel')
+                      ?.display && (
+                      <td align='center'>
+                        {formulateRentSign(
+                          ticker.qttyRent1,
+                          ticker.qttyRent2,
+                          ticker.qttyRent3,
+                          ticker.rentAverage
+                        ) ? (
+                          <MdOutlineCheckCircleOutline
+                            color={colors.green}
+                            size={24}
+                          />
+                        ) : (
+                          <MdOutlineCancel color={colors.red} size={24} />
+                        )}
+                      </td>
+                    )}
 
-                  {fields.find((field) => field.name === 'Magic Formula')
-                    ?.display && (
-                    <td align='center'>
-                      {magicFormulaData.findIndex(
-                        (item) => item.tickerName === ticker.name
-                      ) < 30 ? (
-                        <MdOutlineCheckCircleOutline
-                          color={colors.green}
-                          size={24}
-                        />
-                      ) : (
-                        <MdOutlineCancel color={colors.red} size={24} />
-                      )}
-                    </td>
-                  )}
-                </tr>
-              ))}
+                    {fields.find((field) => field.name === 'Magic Formula')
+                      ?.display && (
+                      <td align='center'>
+                        {magicFormulaData.findIndex(
+                          (item) => item.tickerName === ticker.name
+                        ) < 30 ? (
+                          <MdOutlineCheckCircleOutline
+                            color={colors.green}
+                            size={24}
+                          />
+                        ) : (
+                          <MdOutlineCancel color={colors.red} size={24} />
+                        )}
+                      </td>
+                    )}
+                  </tr>
+                ))}
             </tbody>
           </Table>
         </TableContainer>
