ffc136528fd808d7f4b8581dd723b8453b6306ad
Gabriela Medeiros <gabrielamedeiros998@gmail.com>
2023-02-20 14:53:05 -0400

    add liquidez to magic formula

--- src/utils/magicFormula.ts
+++ src/utils/magicFormula.ts
@@ -1,6 +1,6 @@
-import { IMagicFormula } from "./interfaces";
+import { IMagicFormula, ITicker } from './interfaces';
 
-export const sortRoe = (data: any) => {
+const sortRoe = (data: any) => {
   const result = data
     .map((ticker: { roe: number }) =>
       ticker.roe === undefined ? { ...ticker, roe: 0.0 } : ticker
@@ -10,7 +10,7 @@ export const sortRoe = (data: any) => {
   return result;
 };
 
-export const sortPL = (data: any) => {
+const sortPL = (data: any) => {
   const result = data
     .map((ticker: { pl: number }) =>
       ticker.pl === undefined || ticker.pl < 0 ? { ...ticker, pl: 999 } : ticker
@@ -20,7 +20,7 @@ export const sortPL = (data: any) => {
   return result;
 };
 
-export const sortMargemLiq = (data: any) => {
+const sortMargemLiq = (data: any) => {
   return data
     .map((ticker: { mliquida: number }) =>
       ticker.mliquida > 99 || !ticker?.mliquida
@@ -33,13 +33,27 @@ export const sortMargemLiq = (data: any) => {
     );
 };
 
-export const createMagicFormula = (data: any) => {
+const sortLiquidez = (data: any) => {
+  const result = data
+    .map((ticker: { liquidez: number }) =>
+      ticker.liquidez === undefined ? { ...ticker, liquidez: 0.0 } : ticker
+    )
+    .sort(
+      (a: { liquidez: number }, b: { liquidez: number }) =>
+        a.liquidez - b.liquidez
+    );
+
+  return result;
+};
+
+export const createMagicFormula = (data: ITicker[]) => {
   const roeArray = sortRoe(data);
   const plArray = sortPL(data);
   const mLiquidaArray = sortMargemLiq(data);
+  const liquidezArray = sortLiquidez(data);
   const magicFormula: IMagicFormula[] = [];
 
-  data.forEach((ticker: { name: string }) => {
+  data.forEach((ticker) => {
     const roeIndex = roeArray.findIndex(
       (item: { name: string }) => item.name === ticker.name
     );
@@ -51,7 +65,12 @@ export const createMagicFormula = (data: any) => {
         (item: { name: string }) => item.name === ticker.name
       ) * 0.5;
 
-    const pointsMF = roeIndex + plIndex + mLiquidaIndex;
+    const liquidezIndex =
+      liquidezArray.findIndex(
+        (item: { name: string }) => item.name === ticker.name
+      ) * 0.5;
+
+    const pointsMF = roeIndex + plIndex + mLiquidaIndex + liquidezIndex;
     const tickerName = ticker.name;
     magicFormula.push({ tickerName, pointsMF });
   });
@@ -60,5 +79,6 @@ export const createMagicFormula = (data: any) => {
   const magicFormulaSorted = magicFormula.sort(
     (a, b) => b.pointsMF - a.pointsMF
   );
+
   return magicFormulaSorted;
 };
